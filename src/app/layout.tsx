b65e0c2acda0623f357c60ac252bc5e796eeabf6
Chirayu Sahu <chirayuajaysahu01@gmail.com>
2026-02-13 23:50:06 +0530

    feat: add Toaster component using Sonner for notifications and update package dependencies

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next"
 import { Outfit } from "next/font/google"
 import "./globals.css"
+import { Toaster } from "sonner"
 
 //import sections
 import Navbar from "@/components/sections/navbar"
@@ -25,6 +26,7 @@ export default function RootLayout({
       <body className={`${outfit.variable} font-outfit antialiased`}>
         <Navbar />
         {children}
+        <Toaster position="top-right" />
       </body>
     </html>
   )
